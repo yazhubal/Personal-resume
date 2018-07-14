7d5b459e10d2316ebe837d85340ccfeb4722e712
Piotr Szuszkiewicz - Yazhubal <piotr.szuszkiewicz@gmail.com>
2018-07-14 15:25:43 +0200

    Added  urls to socialmedia icons

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
-function toggleDarkLight() {
-  var body = document.getElementById("body");
-  var currentClass = body.className;
-  body.className = currentClass == "dark-mode" ? "light-mode" : "dark-mode";
-}
+$(document).ready(function(){
+  $(".theme-switcher").click(function(){
+  $(this).text(function(i, v){
+     return v === 'bright' ? 'dark' : 'bright'
+  });
+  });
+});
+
+$(function() {
+$('.theme-switcher').on("click", function(event) {
+  if ($('#theme').hasClass('light')) {
+    $('#theme').removeClass('light');
+    $('#theme').addClass('dark');
+    $('#theme').css({
+      "color": $('#theme').data("styleswitch-color"),
+      "background-color":  $('#theme').data("styleswitch-bgcolor")
+    });
+    $('.linkline').css({
+      "-webkit-text-fill-color": $('.linkline').data("styleswitch-fillcolor") 
+    });
+  } else {
+    $('#theme').addClass('light');
+    $('#theme').removeClass('dark');
+     $('#theme').css({
+      "color": "#333",
+      "background-color": "#fff"
+    });
+    $('.linkline').css({
+      "-webkit-text-fill-color": "#fff"
+    });
+  }
+  event.preventDefault();
+});
+});
