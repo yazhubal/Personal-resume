5b6b27690303e8c56c35ff1a3de8a5872a3eb7ea
Piotr Szuszkiewicz - Yazhubal <piotr.szuszkiewicz@gmail.com>
2018-07-14 15:52:35 +0200

    erase errors

--- script.js
+++ script.js
@@ -1,34 +1,12 @@
-$(document).ready(function(){
-  $(".theme-switcher").click(function(){
-  $(this).text(function(i, v){
-     return v === 'bright' ? 'dark' : 'bright'
-  });
-  });
-});
-
-$(function() {
-$('.theme-switcher').on("click", function(event) {
-  if ($('#theme').hasClass('light')) {
-    $('#theme').removeClass('light');
-    $('#theme').addClass('dark');
-    $('#theme').css({
-      "color": $('#theme').data("styleswitch-color"),
-      "background-color":  $('#theme').data("styleswitch-bgcolor")
-    });
-    $('.linkline').css({
-      "-webkit-text-fill-color": $('.linkline').data("styleswitch-fillcolor") 
-    });
-  } else {
-    $('#theme').addClass('light');
-    $('#theme').removeClass('dark');
-     $('#theme').css({
-      "color": "#333",
-      "background-color": "#fff"
-    });
-    $('.linkline').css({
-      "-webkit-text-fill-color": "#fff"
-    });
+$('#mode').change(function(){   
+    
+  if ($(this).prop('checked'))
+  {
+      $('body').addClass('dark-mode');
   }
-  event.preventDefault();
-});
+  else
+  {
+      $('body').removeClass('dark-mode');
+  }
+  
 });
